61e72a1ad26ecb14409a08f5574a35e037b3f966
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2018-02-24 01:16:56 -0300

    Criacao modulo e component posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostsComponent} from './posts/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
